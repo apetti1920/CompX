12e925b36cf76f5aa9b61656a8ad922bcc3bec47
Aidan Petti <apetti1920@aol.com>
2022-08-28 20:09:34 -0600

    - Got a working dir structure for the loader

--- packages/electron_app/src/index.ts
+++ packages/electron_app/src/index.ts
@@ -1,34 +1,5 @@
 import { app, BrowserWindow, BrowserWindowConstructorOptions } from 'electron';
 import EventEmitter from "events";
-import * as fs from 'fs';
-import https from 'https';
-
-const DownloadFile = (url: string) => {
-    const dest = "file.jpg";
-    const file = fs.createWriteStream(dest);
-    https.get(url, (response) => {
-        response.pipe(file);
-
-        let total = 0;
-        response.on('data', (c) => {
-            total += c.length
-            if (response.headers['content-length'] === undefined) return;
-            const contentLength = parseFloat(response.headers['content-length']);
-            if (contentLength !== undefined && !isNaN(contentLength))
-                loadingEvents.emit('progress', total/contentLength);
-        });
-
-        // after download completed close filestream
-        file.on("finish", () => {
-            file.close();
-            console.log("Download Completed");
-        });
-    }).on('error', err => {
-        fs.unlink(dest, ()=>{
-            console.log(err.message);
-        });
-    });
-}
 
 const SetupApp = async (onProgress: (pct: number)=>void, onFinished: ()=>void) => {
     function sleep(ms: number) {
@@ -46,7 +17,6 @@ const SetupApp = async (onProgress: (pct: number)=>void, onFinished: ()=>void) =
     onFinished();
 }
 
-const loadingEvents = new EventEmitter();
 const CreateWindow = (args?: BrowserWindowConstructorOptions) => new BrowserWindow({
     ...args,
     width: args?.width??900, height: args?.height??600, webPreferences: {
