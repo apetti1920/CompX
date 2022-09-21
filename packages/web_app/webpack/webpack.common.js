8f0c2de3d4490071d7f317a25e5125e006176eeb
Aidan Petti <apetti1920@aol.com>
2022-09-21 15:01:53 -0600

    - test lint 2

--- packages/web_app/webpack/webpack.common.js
+++ packages/web_app/webpack/webpack.common.js
@@ -1,8 +1,8 @@
-
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: path.resolve(__dirname, '..', 'src/index.tsx'),
   resolve: {
