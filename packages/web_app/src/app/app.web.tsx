4518352cd741c32eebe3259a4ca2ffc00dc80da2
Aidan Petti <apetti1920@aol.com>
2023-01-18 23:30:47 -0700

    8669b99d8 Made Electron App Draggable

--- packages/web_app/src/app/app.web.tsx
+++ packages/web_app/src/app/app.web.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 import Container from './Container/Container';
+import './electron-bar.css';
 
 export default function App(): React.ReactElement {
-  console.log(`HEEEEEERE ${process.env.ENV_TYPE} ${process.env.BUILD_TYPE}`);
-  return <Container />;
+  return (
+    <React.Fragment>
+      <Container />
+      {process.env.BUILD_TYPE === 'electron' ? (
+        <div
+          id="electron-drag-bar"
+          style={{ top: 0, width: '100%', height: '30px', position: 'fixed', backgroundColor: 'transparent' }}
+        />
+      ) : (
+        <React.Fragment />
+      )}
+    </React.Fragment>
+  );
 }
