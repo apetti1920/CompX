45e034fbd4584e266cee5dcc0cdf698be78a953e
apetti1920 <apetti1920@aol.com>
2023-01-22 23:50:14 -0700

    Fixed sidbar selecting to show tabs

--- packages/web_app/src/app/Container/Overlay/Tabs/SideTab/SideBar.tsx
+++ packages/web_app/src/app/Container/Overlay/Tabs/SideTab/SideBar.tsx
@@ -1,55 +1,66 @@
 import React, { Component } from 'react';
 import { BatteryCharging, BookOpen, Settings, Trash, User } from 'react-feather';
 
+import LibraryDetailComponent from './Details/LibraryDetailComponent';
 import { NavbarComponent, NavbarType } from './NavbarItemComponent';
+
+const navList: Record<string, NavbarType[]> = {
+  nav1: [
+    { type: 'spacer', height: '15%' },
+    { type: 'icon', icon: BookOpen, openSidebar: { type: 'tab', element: <LibraryDetailComponent /> } },
+    { type: 'icon', icon: Trash, openSidebar: { type: 'tab', element: <React.Fragment /> } },
+    { type: 'icon', icon: BatteryCharging, openSidebar: { type: 'tab', element: <React.Fragment /> } }
+  ],
+  nav2: [
+    { type: 'icon', icon: User, openSidebar: { type: 'tab', element: <React.Fragment /> } },
+    { type: 'icon', icon: Settings, openSidebar: { type: 'tab', element: <React.Fragment /> } }
+  ]
+};
 
 type PropsType = Record<string, never>;
 type StateType = {
-  selected?: string;
+  selected?: {
+    nav: keyof typeof navList;
+    ind: number;
+  };
 };
 
 export default class SideBar extends Component<PropsType, StateType> {
-  private readonly Nav1: NavbarType[];
-  private readonly Nav2: NavbarType[];
   private readonly minimizedWidth: string = '75px';
   private readonly maximizedWidth: string = '350px';
 
   constructor(props: PropsType) {
     super(props);
 
-    this.Nav1 = [
-      { type: 'spacer', height: '15%' },
-      { type: 'icon', icon: BookOpen, openSidebar: { type: 'tab', element: <React.Fragment /> } },
-      { type: 'icon', icon: Trash, openSidebar: { type: 'tab', element: <React.Fragment /> } },
-      { type: 'icon', icon: BatteryCharging, openSidebar: { type: 'tab', element: <React.Fragment /> } }
-    ];
-
-    this.Nav2 = [
-      { type: 'icon', icon: User, openSidebar: { type: 'tab', element: <React.Fragment /> } },
-      { type: 'icon', icon: Settings, openSidebar: { type: 'tab', element: <React.Fragment /> } }
-    ];
-
     this.state = {
       selected: undefined
     };
   }
 
-  selectTabHandler = (tabName?: string) => {
-    if (tabName === undefined) return;
-    if (this.state.selected !== undefined && this.state.selected === tabName) this.setState({ selected: undefined });
-    else this.setState({ selected: tabName });
+  selectTabHandler = (props: typeof this.state.selected) => {
+    if (props === undefined) return;
+
+    if (
+      this.state.selected !== undefined &&
+      this.state.selected.nav === props.nav &&
+      this.state.selected.ind === props.ind
+    )
+      this.setState({ selected: undefined });
+    else this.setState({ selected: props });
   };
 
-  NavWrap = (nav: NavbarType[]): React.ReactElement[] =>
-    nav.map((t, ind) => {
+  NavWrap = (nav: keyof typeof navList, listNav: NavbarType[]): React.ReactElement[] =>
+    listNav.map((t, ind) => {
       switch (t.type) {
         case 'icon': {
           return (
             <NavbarComponent
               key={`icon_${t.icon.displayName}`}
               icon={t.icon}
-              onCLick={() => this.selectTabHandler(t.icon.displayName)}
-              isSelected={this.state.selected !== undefined && t.icon.displayName === this.state.selected}
+              onCLick={() => this.selectTabHandler({ nav: nav, ind: ind })}
+              isSelected={
+                this.state.selected !== undefined && this.state.selected.nav === nav && this.state.selected.ind === ind
+              }
             />
           );
         }
@@ -63,6 +74,26 @@ export default class SideBar extends Component<PropsType, StateType> {
     });
 
   render() {
+    let OpenComp = <React.Fragment />;
+    if (this.state.selected !== undefined) {
+      const openCompTmp = navList[this.state.selected.nav][this.state.selected.ind];
+      if (openCompTmp.type === 'icon' && openCompTmp.openSidebar.type === 'tab') {
+        OpenComp = (
+          <div
+            style={{
+              borderLeft: '1px solid black',
+              width: '100%',
+              height: '100%',
+              padding: '15px',
+              paddingTop: '24px'
+            }}
+          >
+            {openCompTmp.openSidebar.element}
+          </div>
+        );
+      }
+    }
+
     return (
       <div
         style={{
@@ -74,19 +105,15 @@ export default class SideBar extends Component<PropsType, StateType> {
       >
         <div style={{ display: 'flex', flexFlow: 'column nowrap', width: this.minimizedWidth, height: '100%' }}>
           <div style={{ height: '75%', width: this.minimizedWidth, display: 'flex', flexFlow: 'column nowrap' }}>
-            {this.NavWrap(this.Nav1)}
+            {this.NavWrap('nav1', navList.nav1)}
           </div>
           <div
             style={{ height: '25%', width: this.minimizedWidth, display: 'flex', flexFlow: 'column-reverse nowrap' }}
           >
-            {this.NavWrap(this.Nav2)}
+            {this.NavWrap('nav2', navList.nav2)}
           </div>
         </div>
-        {this.state.selected !== undefined ? (
-          <div style={{ borderLeft: '1px solid black', width: '100%', height: '100%' }} />
-        ) : (
-          <React.Fragment />
-        )}
+        {OpenComp}
       </div>
     );
   }
