66bc706c73a9579b2af32ac513fc57bbcfce7906
apetti1920 <apetti1920@aol.com>
2023-01-29 23:48:32 -0700

    Started Search Bar

--- packages/web_app/src/app/Container/Overlay/Tabs/SideTab/Details/LibraryDetailComponent.tsx
+++ packages/web_app/src/app/Container/Overlay/Tabs/SideTab/Details/LibraryDetailComponent.tsx
@@ -1,5 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Search, XSquare } from 'react-feather';
+// import { throttle } from 'lodash';
 
-export default function LibraryDetailComponent() {
-  return <div style={{ width: '100%', height: '100%' }}>Testing Testing</div>;
+type State = {
+  searchText: {
+    value: string;
+    throttledValue: string;
+  };
+};
+
+export default class LibraryDetailComponent extends Component<never, State> {
+  constructor(props: never) {
+    super(props);
+
+    this.state = {
+      searchText: {
+        value: '',
+        throttledValue: ''
+      }
+    };
+  }
+
+  // handleInputChangeThrottled = throttle((text: string) => {
+  //   this.setState(
+  //     (prevState: State) => ({ searchText: { ...prevState.searchText, throttledValue: text } }),
+  //     console.log(this.state.searchText.throttledValue)
+  //   );
+  // }, 1000);
+
+  // componentDidUpdate(prevProps: never, prevState: State) {
+  //   if (this.state.searchText.value !== prevState.searchText.value) {
+  //     this.setState({ searchText: { ...prevState.searchText, throttledValue: prevState.searchText.value } });
+  //   }
+  // }
+
+  render() {
+    return (
+      <div style={{ padding: '12px', backgroundColor: '#696969', borderRadius: '20px', width: '100%' }}>
+        <div style={{ display: 'flex', flexFlow: 'row nowrap' }}>
+          <style>{`
+            input:focus,
+            select:focus,
+            textarea:focus,
+            button:focus {
+                outline: none;
+            },
+            .blockSearchInput::placeholder {
+              color: #BEBEBE;
+              opacity: 0.4;
+            }
+        `}</style>
+          <Search style={{ marginRight: '5px', color: '#BEBEBE' }} />
+          <input
+            className="blockSearchInput"
+            type="text"
+            placeholder="Block Search"
+            value={this.state.searchText.value}
+            onChange={(evt) =>
+              this.setState((prevState) => ({ searchText: { ...prevState.searchText, value: evt.target.value } }))
+            }
+            style={{ width: '100%', background: 'transparent', border: 'none' }}
+          />
+          {this.state.searchText.value !== '' ? (
+            <XSquare
+              onClick={() => this.setState({ searchText: { value: '', throttledValue: '' } })}
+              style={{ marginLeft: '5px', color: '#CC5500' }}
+            />
+          ) : (
+            <React.Fragment />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
