7f4623a8cb023c1f6d1fc58197389f55a55352d7
apetti1920 <apetti1920@aol.com>
2023-03-06 23:38:03 -0700

    converting colors

--- packages/web_app/src/app/app.web.tsx
+++ packages/web_app/src/app/app.web.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
-import Container from './Container/Container';
+// import Container from './Container/Container';
 import './electron-bar.css';
+import Color from 'values.js';
 
 export default function App(): React.ReactElement {
+  const col = new Color('#232946');
+
   return (
     <React.Fragment>
-      <Container />
+      {/* <Container /> */}
+      <div style={{ display: 'flex', flexFlow: 'column nowrap', width: '50vw', height: '500px' }}>
+        <div style={{ backgroundColor: col.hexString(), width: '100%', height: '20%', margin: '5px' }} />
+        <div style={{ backgroundColor: col.tint(25).hexString(), width: '100%', height: '20%', margin: '5px' }} />
+        <div style={{ backgroundColor: col.tint(50).hexString(), width: '100%', height: '20%', margin: '5px' }} />
+        <div style={{ backgroundColor: col.tint(75).hexString(), width: '100%', height: '20%', margin: '5px' }} />
+        <div style={{ backgroundColor: col.tint(100).hexString(), width: '100%', height: '20%', margin: '5px' }} />
+      </div>
+
       {process.env.BUILD_TYPE === 'electron' ? (
         <div
           id="electron-drag-bar"
--- packages/web_app/src/theme/ColorTheme.ts
+++ packages/web_app/src/theme/ColorTheme.ts
@@ -1,96 +1,23 @@
-import { ThemeType } from '../types';
+import Color from 'values.js';
 
-type RGBColorType = { r: number; g: number; b: number; a: number | undefined };
-type HSLColorType = { h: number; s: number; l: number };
+import { ThemeStorageType } from '../types';
 
-class Color {
-  private readonly value: RGBColorType;
+type ThemeType = {
+  [P in keyof ThemeStorageType]: { [U in keyof ThemeStorageType[P]]: Color };
+};
 
-  private constructor(r: number, g: number, b: number, a: number | undefined) {
-    this.value = { r, g, b, a };
-  }
-
-  public static fromHexString(color: string): Color {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i.exec(color);
-    if (result === null) throw Error('Correct Color format not supplied');
-
-    return new Color(
-      parseInt(result[1], 16),
-      parseInt(result[2], 16),
-      parseInt(result[3], 16),
-      parseInt(result[4], 16) || undefined
-    );
-  }
-
-  public static fromHSL(color: HSLColorType): Color {
-    let r;
-    let g;
-    let b;
-
-    if (color.s === 0) {
-      // eslint-disable-next-line no-multi-assign
-      r = g = b = color.l; // achromatic
-    } else {
-      const hue2rgb = function hue2rgb(p: number, q: number, t: number) {
-        let tmpT = t;
-
-        if (tmpT < 0) tmpT += 1;
-        if (tmpT > 1) tmpT -= 1;
-        if (tmpT < 1 / 6) return p + (q - p) * 6 * tmpT;
-        if (tmpT < 1 / 2) return q;
-        if (tmpT < 2 / 3) return p + (q - p) * (2 / 3 - tmpT) * 6;
-        return p;
-      };
-
-      const q = color.l < 0.5 ? color.l * (1 + color.s) : color.l + color.s - color.l * color.s;
-      const p = 2 * color.l - q;
-      r = hue2rgb(p, q, color.h + 1 / 3);
-      g = hue2rgb(p, q, color.h);
-      b = hue2rgb(p, q, color.h - 1 / 3);
-    }
-
-    return new Color(Math.round(r * 255), Math.round(g * 255), Math.round(b * 255), 1.0);
-  }
-
-  public static fromRGB(r: number, g: number, b: number, a: number | undefined) {
-    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
-      throw Error(`One or more supplied channel values is not between 0 and 255`);
-
-    return new Color(r, g, b, a);
-  }
-
-  public getRGB(): RGBColorType {
-    return this.value;
-  }
-
-  public getHexString(): string {
-    return `#${Object.values(this.value).map((x) => {
-      if (x === undefined) return;
-
-      const xStr = x.toString(16);
-      // eslint-disable-next-line consistent-return
-      return xStr.length === 1 ? `0${xStr}` : xStr;
-    })}`;
-  }
-
-  public getShade(shadeFactor: number): Color {
-    return new Color(
-      this.value.r * (1 - shadeFactor),
-      this.value.g * (1 - shadeFactor),
-      this.value.b * (1 - shadeFactor),
-      this.value.a
-    );
-  }
-}
-
-class ColorTheme {
-  private readonly baseColors: ThemeType;
-
-  constructor(baseColors: ThemeType) {
-    this.baseColors = baseColors;
-  }
-
-  public GetColors(): Readonly<ThemeType> {
-    return this.baseColors;
+export default class ColorTheme {
+  private readonly theme: ThemeType;
+  constructor(theme: ThemeStorageType) {
+    this.theme = Object.fromEntries(
+      Object.keys(theme).map((k) => [
+        k,
+        Object.fromEntries(
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          Object.keys(theme[k as unknown as keyof ThemeStorageType]).map((k2) => [k2, new Color(theme[k][k2])])
+        )
+      ])
+    ) as ThemeType;
   }
 }
