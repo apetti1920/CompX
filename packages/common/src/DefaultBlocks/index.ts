eb9f672d5570140ae7d7c805838e0f7ef05c94fa
apetti1920 <apetti1920@aol.com>
2025-10-29 00:16:46 -0600

    remove old packages

--- packages/common/src/DefaultBlocks/index.ts
+++
@@ -1,8 +0,0 @@
-import Constant from './Constant';
-import Gain from './Gain';
-import Integrator from './Integrator';
-import Multiply from './Multiply';
-import Scope from './Scope';
-import Sum from './Sum';
-
-export { Constant, Sum, Scope, Gain, Integrator, Multiply };
